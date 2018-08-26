e5bb7f5b26659d8c5be6e4e69abab9baca58ebec
seLain <selain@nature.ee.ncku.edu.tw>
2018-08-26 10:37:30 +0800

    fix for travisCI

--- tests/behavior_tests/test_scheduler.py
+++ tests/behavior_tests/test_scheduler.py
@@ -10,15 +10,15 @@ def test_bot_reply_specific_time(driver):
     str_t_time = t_time.strftime('%b-%d-%Y_%H:%M:%S')
     driver.send_direct_message(
         'reply "{0}" at {0}'.format(str_t_time), tobot=True)
-    driver.wait_for_bot_direct_message(str_t_time)
+    driver.wait_for_bot_direct_message(str_t_time, maxwait=60)
     settings.JOB_TRIGGER_PERIOD = 60
 
 
 def test_bot_reply_every_seconds(driver):
     settings.JOB_TRIGGER_PERIOD = 2
     driver.send_direct_message('reply "alive" every 5 seconds', tobot=True)
-    driver.wait_for_bot_direct_message('alive')
-    driver.wait_for_bot_direct_message('alive')
+    driver.wait_for_bot_direct_message('alive', maxwait=60)
+    driver.wait_for_bot_direct_message('alive', maxwait=60)
     driver.send_direct_message('cancel jobs', tobot=True)
-    driver.wait_for_bot_direct_message('all jobs canceled.')
+    driver.wait_for_bot_direct_message('all jobs canceled.', maxwait=60)
     settings.JOB_TRIGGER_PERIOD = 60
