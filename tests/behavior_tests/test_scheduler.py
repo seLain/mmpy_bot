345cfca74d57933ac1423900084ac643e6dfdc6e
seLain <selain@nature.ee.ncku.edu.tw>
2018-08-26 09:49:47 +0800

    fix

--- tests/behavior_tests/test_scheduler.py
+++ tests/behavior_tests/test_scheduler.py
@@ -1,18 +1,24 @@
 from datetime import datetime, timedelta
 from tests.behavior_tests.fixture import driver
 
+from mmpy_bot import settings
+
 
 def test_bot_reply_specific_time(driver):
+    settings.JOB_TRIGGER_PERIOD = 2
     t_time = datetime.now() + timedelta(seconds=5)
     str_t_time = t_time.strftime('%b-%d-%Y_%H:%M:%S')
     driver.send_direct_message(
         'reply "{0}" at {0}'.format(str_t_time), tobot=True)
     driver.wait_for_bot_direct_message(str_t_time)
+    settings.JOB_TRIGGER_PERIOD = 60
 
 
 def test_bot_reply_every_seconds(driver):
+    settings.JOB_TRIGGER_PERIOD = 2
     driver.send_direct_message('reply "alive" every 5 seconds', tobot=True)
     driver.wait_for_bot_direct_message('alive')
     driver.wait_for_bot_direct_message('alive')
     driver.send_direct_message('cancel jobs', tobot=True)
     driver.wait_for_bot_direct_message('all jobs canceled.')
+    settings.JOB_TRIGGER_PERIOD = 60
